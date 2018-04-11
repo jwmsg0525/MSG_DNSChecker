302d0c6340ff58faa4d89d932f73a07775cd0ad6
jwmsg0525 <jwmsg0525@gmail.com>
2018-04-11 20:51:16 +0900

    Update senddns.cpp

--- DNSchecker/senddns.cpp
+++ DNSchecker/senddns.cpp
@@ -12,25 +12,25 @@ SendDNS::SendDNS(QObject *parent) : QThread(parent)
 }
 
 void SendDNS::run(){
-while(threadrun){
-NetworkInterface interface = NetworkInterface::default_interface();
-PacketSender sender;
-IP ip1 = IP(dns1);
-IP ip2 = IP(dns2);
-UDP udp = UDP(53,1337);
-DNS dns = DNS();
-dns.add_query({domain,DNS::A, DNS::IN});
-dns.recursion_desired(1);
-udp.inner_pdu(dns);
-ip1.inner_pdu(udp);
-ip2.inner_pdu(udp);
+    while(threadrun){
+        NetworkInterface interface = NetworkInterface::default_interface();
+        PacketSender sender;
+        IP ip1 = IP(dns1);
+        IP ip2 = IP(dns2);
+        UDP udp = UDP(53,1337);
+        DNS dns = DNS();
+        dns.add_query({domain,DNS::A, DNS::IN});
+        dns.recursion_desired(1);
+        udp.inner_pdu(dns);
+        ip1.inner_pdu(udp);
+        ip2.inner_pdu(udp);
 
-std::thread thr(&ReciveDNS::recive,ReciveDNS(log_locate));
-sender.send(ip1,interface.name());
-sender.send(ip2,interface.name());
-thr.join();
-sleep(sec);
-}
+        std::thread thr(&ReciveDNS::recive,ReciveDNS(log_locate));
+        sender.send(ip1,interface.name());
+        sender.send(ip2,interface.name());
+        thr.join();
+        sleep(sec);
+    }
 }
 
 void SendDNS::Setting(string domain, string dns1, string dns2, string log_locate, int sec){
